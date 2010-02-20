89bfdc0dafe70f893c55f7b7fba6b4f9e6b0ff63
juilin.sandar <juilin.sandar@3982c302-c227-11de-8ec6-755e00613368>
2010-02-20 05:23:42 +0000

    M    Sensorium2/UdpPlugin/UdpPluginClient.cs

--- trunk/Sensorium2/UdpPlugin/UdpPluginClient.cs
+++ trunk/Sensorium2/UdpPlugin/UdpPluginClient.cs
@@ -10,8 +10,9 @@
  *	A PARTICULAR PURPOSE. See the GNU General Public License 
  *	for more details. You should have received a copy of the GNU General 
  *	Public License along with this program. If not, see <http://www.gnu.org/licenses/>
-*/
+ */
 
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
