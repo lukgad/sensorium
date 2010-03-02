6fd222c0235d4f20ea7ea2b4cc87db0d76143bb9
juilin.sandar <juilin.sandar@3982c302-c227-11de-8ec6-755e00613368>
2010-03-02 05:25:50 +0000

    M    Sensorium2/libSensorsPlugin/libSensorsWrapper.cs

--- trunk/Sensorium2/libSensorsPlugin/libSensorsWrapper.cs
+++ trunk/Sensorium2/libSensorsPlugin/libSensorsWrapper.cs
@@ -374,7 +374,8 @@ namespace libSensorsPlugin {
 // ReSharper restore UnusedMember.Local
 // ReSharper restore InconsistentNaming
 
-		private readonly Dictionary<IntPtr, Dictionary<IntPtr, List<IntPtr>>> _chips;
+		private readonly Dictionary<IntPtr, Dictionary<IntPtr, List<IntPtr>>> _chips = 
+			new Dictionary<IntPtr, Dictionary<IntPtr, List<IntPtr>>>();
 
 		public LibSensorsWrapper() {
 			if(Environment.OSVersion.Platform != PlatformID.Unix)
@@ -383,27 +384,27 @@ namespace libSensorsPlugin {
 			IntPtr filePtr = NativeMethods.fopen(libsensors_conf, "r");
 			if(NativeMethods.sensors_init(filePtr) != 0)
 				throw new InvalidDataException();
-			NativeMethods.fclose(filePtr);
+			//NativeMethods.fclose(filePtr);
 
 			int cnr = 0;
 			IntPtr chipNamePtr;
             while((chipNamePtr = NativeMethods.sensors_get_detected_chips(IntPtr.Zero, ref cnr)) != IntPtr.Zero) {
-				sensors_chip_name chipName = (sensors_chip_name)
-					Marshal.PtrToStructure(chipNamePtr, typeof(sensors_chip_name));
+				//sensors_chip_name chipName = (sensors_chip_name)
+				//	Marshal.PtrToStructure(chipNamePtr, typeof(sensors_chip_name));
 				
 				_chips.Add(chipNamePtr,new Dictionary<IntPtr, List<IntPtr>>());
 
             	int fnr = 0;
             	IntPtr mainFeaturePtr;
 				while((mainFeaturePtr = NativeMethods.sensors_get_features(chipNamePtr, ref fnr)) != IntPtr.Zero) {
-					sensors_feature mainFeature = 
-						(sensors_feature)Marshal.PtrToStructure(mainFeaturePtr, typeof(sensors_feature));
-
+					//sensors_feature mainFeature = 
+					//	(sensors_feature)Marshal.PtrToStructure(mainFeaturePtr, typeof(sensors_feature));
+					
 					_chips[chipNamePtr].Add(mainFeaturePtr, new List<IntPtr>());
 
 					int sfnr = 0;
 					IntPtr subFeaturePtr;
-					while ((subFeaturePtr = NativeMethods.sensors_get_all_subfeatures(IntPtr.Zero, mainFeaturePtr,
+					while ((subFeaturePtr = NativeMethods.sensors_get_all_subfeatures(chipNamePtr, mainFeaturePtr,
 						ref sfnr)) != IntPtr.Zero) {
 						_chips[chipNamePtr][mainFeaturePtr].Add(subFeaturePtr);
 					}
@@ -416,7 +417,14 @@ namespace libSensorsPlugin {
 					Console.WriteLine(" " + ((sensors_feature) 
 						Marshal.PtrToStructure(f, typeof (sensors_feature))).name);
 					foreach (IntPtr sf in _chips[cn][f]) {
-						Console.WriteLine("  " + ((sensors_subfeature) Marshal.PtrToStructure(sf, typeof (sensors_subfeature))).name);
+						double value = 0;
+						if (NativeMethods.sensors_get_value(cn,	((sensors_subfeature)
+							Marshal.PtrToStructure(sf, typeof (sensors_subfeature))).number,ref value) != 0)
+							Console.WriteLine("ERROR");
+						
+						Console.WriteLine("  " + ((sensors_subfeature) 
+						                          Marshal.PtrToStructure(sf, typeof (sensors_subfeature))).name + 
+						                  " " + value);
 					}
 				}
 			}
