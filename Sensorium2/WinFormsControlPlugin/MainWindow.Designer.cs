bd8b0600883207a1ecaea0ec28d57fa44ace5b72
juilin.sandar <juilin.sandar@3982c302-c227-11de-8ec6-755e00613368>
2010-03-15 12:42:18 +0000

    WinFormsControlPlugin: Moved refresh button to toolbar. Button is hidden when tab is hidden.

--- Sensorium2/WinFormsControlPlugin/MainWindow.Designer.cs
+++ Sensorium2/WinFormsControlPlugin/MainWindow.Designer.cs
@@ -37,95 +37,111 @@ namespace WinFormsControlPlugin {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.mainTabs = new System.Windows.Forms.TabControl();
-			this.sensorsTab = new System.Windows.Forms.TabPage();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.refreshButton = new System.Windows.Forms.ToolStripButton();
 			this.logTab = new System.Windows.Forms.TabPage();
 			this.logListBox = new System.Windows.Forms.ListBox();
-			this.refreshLog = new System.Windows.Forms.Button();
-			this.mainTabs.SuspendLayout();
+			this.sensorsTab = new System.Windows.Forms.TabPage();
+			this.mainTabs = new System.Windows.Forms.TabControl();
+			this.toolStrip1.SuspendLayout();
 			this.logTab.SuspendLayout();
+			this.mainTabs.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// mainTabs
+			// toolStrip1
 			// 
-			this.mainTabs.Controls.Add(this.sensorsTab);
-			this.mainTabs.Controls.Add(this.logTab);
-			this.mainTabs.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.mainTabs.Location = new System.Drawing.Point(0, 0);
-			this.mainTabs.Name = "mainTabs";
-			this.mainTabs.SelectedIndex = 0;
-			this.mainTabs.Size = new System.Drawing.Size(517, 391);
-			this.mainTabs.TabIndex = 0;
+			this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshButton});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(517, 25);
+			this.toolStrip1.TabIndex = 0;
 			// 
-			// sensorsTab
+			// refreshButton
 			// 
-			this.sensorsTab.Location = new System.Drawing.Point(4, 22);
-			this.sensorsTab.Name = "sensorsTab";
-			this.sensorsTab.Padding = new System.Windows.Forms.Padding(3);
-			this.sensorsTab.Size = new System.Drawing.Size(376, 236);
-			this.sensorsTab.TabIndex = 0;
-			this.sensorsTab.Text = "Sensors";
-			this.sensorsTab.UseVisualStyleBackColor = true;
+			this.refreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.refreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.refreshButton.Name = "refreshButton";
+			this.refreshButton.Size = new System.Drawing.Size(50, 22);
+			this.refreshButton.Text = "Refresh";
+			this.refreshButton.ToolTipText = "Refresh";
+			this.refreshButton.Visible = false;
+			this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
 			// 
 			// logTab
 			// 
-			this.logTab.Controls.Add(this.refreshLog);
 			this.logTab.Controls.Add(this.logListBox);
 			this.logTab.Location = new System.Drawing.Point(4, 22);
 			this.logTab.Name = "logTab";
 			this.logTab.Padding = new System.Windows.Forms.Padding(3);
-			this.logTab.Size = new System.Drawing.Size(509, 365);
+			this.logTab.Size = new System.Drawing.Size(509, 363);
 			this.logTab.TabIndex = 1;
 			this.logTab.Text = "Application Log";
 			this.logTab.UseVisualStyleBackColor = true;
 			// 
 			// logListBox
 			// 
-			this.logListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-						| System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.logListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.logListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.logListBox.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.logListBox.FormattingEnabled = true;
 			this.logListBox.HorizontalScrollbar = true;
-			this.logListBox.Location = new System.Drawing.Point(8, 6);
+			this.logListBox.Location = new System.Drawing.Point(3, 3);
 			this.logListBox.Name = "logListBox";
-			this.logListBox.Size = new System.Drawing.Size(493, 327);
+			this.logListBox.Size = new System.Drawing.Size(503, 351);
 			this.logListBox.TabIndex = 0;
 			// 
-			// refreshLog
+			// sensorsTab
 			// 
-			this.refreshLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.refreshLog.Location = new System.Drawing.Point(439, 337);
-			this.refreshLog.Name = "refreshLog";
-			this.refreshLog.Size = new System.Drawing.Size(62, 20);
-			this.refreshLog.TabIndex = 1;
-			this.refreshLog.Text = "Refresh";
-			this.refreshLog.UseVisualStyleBackColor = true;
-			this.refreshLog.Click += new System.EventHandler(this.refreshLog_Click);
+			this.sensorsTab.Location = new System.Drawing.Point(4, 22);
+			this.sensorsTab.Name = "sensorsTab";
+			this.sensorsTab.Padding = new System.Windows.Forms.Padding(3);
+			this.sensorsTab.Size = new System.Drawing.Size(509, 363);
+			this.sensorsTab.TabIndex = 0;
+			this.sensorsTab.Text = "Sensors";
+			this.sensorsTab.UseVisualStyleBackColor = true;
+			// 
+			// mainTabs
+			// 
+			this.mainTabs.Controls.Add(this.sensorsTab);
+			this.mainTabs.Controls.Add(this.logTab);
+			this.mainTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.mainTabs.Location = new System.Drawing.Point(0, 25);
+			this.mainTabs.Multiline = true;
+			this.mainTabs.Name = "mainTabs";
+			this.mainTabs.SelectedIndex = 0;
+			this.mainTabs.Size = new System.Drawing.Size(517, 389);
+			this.mainTabs.TabIndex = 1;
+			this.mainTabs.SelectedIndexChanged += new System.EventHandler(this.mainTabs_SelectedIndexChanged);
 			// 
 			// MainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(517, 391);
+			this.ClientSize = new System.Drawing.Size(517, 414);
 			this.Controls.Add(this.mainTabs);
+			this.Controls.Add(this.toolStrip1);
 			this.Name = "MainWindow";
 			this.Text = "Sensorium2";
 			this.Load += new System.EventHandler(this.MainWindow_Load);
-			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
-			this.mainTabs.ResumeLayout(false);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
 			this.logTab.ResumeLayout(false);
+			this.mainTabs.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.TabControl mainTabs;
-		private System.Windows.Forms.TabPage sensorsTab;
+		private System.Windows.Forms.ToolStrip toolStrip1;
 		private System.Windows.Forms.TabPage logTab;
 		private System.Windows.Forms.ListBox logListBox;
-		private System.Windows.Forms.Button refreshLog;
+		private System.Windows.Forms.TabPage sensorsTab;
+		private System.Windows.Forms.TabControl mainTabs;
+		private System.Windows.Forms.ToolStripButton refreshButton;
+
 
 
 
