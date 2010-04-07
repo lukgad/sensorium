f95613c5e6dd8a8b5f5cd85fe90d5f215d19cb81
juilin.sandar <juilin.sandar@3982c302-c227-11de-8ec6-755e00613368>
2010-04-07 12:00:08 +0000

    You can now disable plugins from the winforms control plugin

--- Sensorium2/WinFormsControlPlugin/MainWindow.cs
+++ Sensorium2/WinFormsControlPlugin/MainWindow.cs
@@ -25,6 +25,8 @@ namespace WinFormsControlPlugin {
 
 		private void MainWindow_Load(object sender, EventArgs e) {
 			Text = "Sensorium2 v" + SensoriumFactory.GetAppInterface().Version;
+			
+			
 		}
 
 		public new void Close() {
@@ -37,19 +39,67 @@ namespace WinFormsControlPlugin {
 		}
 
 		private void mainTabs_SelectedIndexChanged(object sender, EventArgs e) {
-			refreshButton.Visible = logTab.Visible;
+			refreshButton.Visible = logTab.Visible || pluginsTab.Visible;
 		}
 
 		private void refreshButton_Click(object sender, EventArgs e) {
-			logListBox.BeginUpdate();
-			logListBox.Items.Clear();
+			if (logTab.Visible) {
+				logListBox.BeginUpdate();
+				logListBox.Items.Clear();
 
-			foreach (LoggingEvent le in SensoriumFactory.GetAppInterface().Log.GetEvents()) {
-				logListBox.Items.Add(String.Format("[{0}] {1} {2} - {3}", le.GetLoggingEventData().TimeStamp,
-					le.GetLoggingEventData().Level, le.GetLoggingEventData().LoggerName, le.GetLoggingEventData().Message));
+				foreach (LoggingEvent le in SensoriumFactory.GetAppInterface().Log.GetEvents()) {
+					logListBox.Items.Add(String.Format("[{0}] {1} {2} - {3}", le.GetLoggingEventData().TimeStamp,
+													   le.GetLoggingEventData().Level, le.GetLoggingEventData().LoggerName,
+													   le.GetLoggingEventData().Message));
+				}
+
+				logListBox.EndUpdate();
+			} else {
+				pluginsListView.BeginUpdate();
+
+				pluginsListView.Items.Clear();
+
+				foreach (string p in SensoriumFactory.GetAppInterface().Plugins.Keys)
+				{
+					ListViewItem listItem = new ListViewItem(SensoriumFactory.GetAppInterface().Plugins[p].Name,
+					                                         SensoriumFactory.GetAppInterface().Plugins[p].Enabled
+					                                         	? "plugin"
+					                                         	: "plugin_disabled");
+					listItem.SubItems.Add(new ListViewItem.ListViewSubItem(listItem,
+					                                                       SensoriumFactory.GetAppInterface().Plugins[p].Enabled.
+					                                                       	ToString()));
+					pluginsListView.Items.Add(listItem);
+				}
+
+				pluginsListView.EndUpdate();
 			}
+		}
 
-			logListBox.EndUpdate();
+		private void pluginsListView_SelectedIndexChanged(object sender, EventArgs e) {
+			if (pluginsListView.SelectedItems.Count == 1) {
+				enableButton.Enabled = !SensoriumFactory.GetAppInterface().Plugins[pluginsListView.SelectedItems[0].Text].Enabled;
+				disableButton.Enabled = SensoriumFactory.GetAppInterface().Plugins[pluginsListView.SelectedItems[0].Text].Enabled;
+			}
+			else if (pluginsListView.SelectedItems.Count == 0) 
+				enableButton.Enabled = disableButton.Enabled = false;
+			else enableButton.Enabled = disableButton.Enabled = true;
+		}
+
+		private void enableButton_Click(object sender, EventArgs e) {
+			foreach(ListViewItem i in pluginsListView.SelectedItems) {
+				if(SensoriumFactory.GetAppInterface().Plugins[i.Text].Enabled)
+					continue;
+			}
+		}
+
+		private void disableButton_Click(object sender, EventArgs e) {
+			foreach (ListViewItem i in pluginsListView.SelectedItems) {
+				if (!SensoriumFactory.GetAppInterface().Plugins[i.Text].Enabled)
+					continue;
+
+				SensoriumFactory.GetAppInterface().Plugins[i.Text].Enabled = false;
+				SensoriumFactory.GetAppInterface().Plugins[i.Text].Stop();
+			}
 		}
 	}
 }
